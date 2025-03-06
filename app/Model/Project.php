a9d8c3cd9bbd631843f9659cb4141028ec96f9e8
RivoLink <rivo.link@gmail.com>
2025-03-06 07:52:16 +0300

    chore: update model Project

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -12,6 +12,27 @@ class Project
         return (string)($_ENV[$key] ?? $default); // @phpstan-ignore-line
     }
 
+    /**
+     * @return array<int, list<string>|null>
+     */
+    public static function exec(string $cmd, ?callable $callback = null): array {
+        $output = null;
+        $return = null;
+
+        $path = self::dir();
+
+        exec("cd {$path} && {$cmd} 2>&1", $output, $return);
+
+        $result = ($return === 0) ? $output : null;
+        $errors = ($return === 0) ? null : $output;
+
+        if ($callback) {
+            $callback($result, $errors);
+        }
+
+        return [$result, $errors];
+    }
+
     public static function set(string $path, string $key, string $newValue): void {
         /** @var list<string> */
         $lines = file($path, FILE_IGNORE_NEW_LINES);
